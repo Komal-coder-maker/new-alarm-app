b7e18ed59832d7a0d972da1135d01a26649d6357
Komal-coder-maker <kr730936@gmail.com>
2026-04-07 13:05:37 +0530

    finally my alarm -app completed

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'alarm-app-v1';
+const ASSETS = [
+  './',
+  'index.html',
+  'manifest.json',
+  'app.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate & Clean old cache
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Handle Update Message
+self.addEventListener('message', (event) => {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
